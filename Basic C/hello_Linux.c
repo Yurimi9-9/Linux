3e15b74b5aed530e83215491f62de59d44fb5bff
Yurimi <59168302+Yurimi9-9@users.noreply.github.com>
2020-07-10 17:31:19 +0900

    Create hello_Linux.c
    Basic Structure of C

---
+++ Basic C/hello_Linux.c
@@ -0,0 +1,15 @@
+/* Basic Structure of C Programming */
+#include <stdio.h>              // 헤더파일 : stdio(=Standard Input Output)
+
+int main()                      // main 함수
+{
+    printf("Hello Linux\n");    // 문자열 출력 (\n: Enter)
+    printf("My name is ");      // \n이 없으면 줄바꿈 X
+    printf("Yurimi\n");
+}
+
+/*
+*   [ 출력 ]
+*   Hello Linux
+*   My name is Yurimi
+*/
