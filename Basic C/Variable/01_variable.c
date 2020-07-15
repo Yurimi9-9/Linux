e7533b06bac9d45531b76bfa64dea43f342e9bce
Yurimi <59168302+Yurimi9-9@users.noreply.github.com>
2020-07-15 23:55:00 +0900

    Create 01_variable.c

---
+++ Basic C/Variable/01_variable.c
@@ -0,0 +1,15 @@
+/* Basic Variable */
+#include <stdio.h>
+
+int main()
+{
+        int result;    // 변수 선언
+
+        result = 4 + 8;
+        printf("4 + 6 = %d\n", result); 
+}
+
+/* 
+*   [ 출력 ]
+*   4 + 8 = 12
+*/
