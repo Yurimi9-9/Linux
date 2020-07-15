3babfd829ca14d7d19e81c56396d6fa0ab38a02d
Yurimi <59168302+Yurimi9-9@users.noreply.github.com>
2020-07-15 17:22:13 +0900

    Create 04_string_constant.c

---
+++ Basic C/04_string_constant.c
@@ -0,0 +1,9 @@
+/* String Constant - 문자열 상수 */
+#include <stdio.h>
+
+int main()
+{
+        printf("---- String ----\n");
+        printf("%s\n", "Linux");
+        printf("%s\n", "C Programming");
+}
