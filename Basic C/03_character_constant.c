911314e1124e76ed6a5b538b638d93647d96d609
Yurimi <59168302+Yurimi9-9@users.noreply.github.com>
2020-07-15 17:21:22 +0900

    Create 03_character_constant.c

---
+++ Basic C/03_character_constant.c
@@ -0,0 +1,14 @@
+/* Character Constant - 문자 상수 */
+#include <stdio.h>
+
+int main()
+{
+        printf("---- character ----\n");
+        printf("100:\t%c\n", 100);
+        printf("70:\t%c\n", 70);
+        printf("---- integer ----\n");
+        printf("'A':\t%d\n", 'A');
+        printf("'Z':\t%d\n", 'Z');
+        printf("'a':\t%d\n", 'a');
+        printf("'z':\t%d\n", 'z');
+}
