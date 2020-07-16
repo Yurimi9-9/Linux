edf186404c48f892aa839ca456d8589200191ae5
Yurimi <59168302+Yurimi9-9@users.noreply.github.com>
2020-07-16 20:27:16 +0900

    Create 01_variable2.c

---
+++ Basic C/Variable/01_variable2.c
@@ -0,0 +1,17 @@
+/* Basic Variable 2 */
+#include <stdio.h>
+
+int main()
+{
+        int i = 5;
+        int j = 10;
+
+        printf("i = %d\n", i);
+        printf("j = %d\n", j);
+}
+
+/* 
+*   [ 출력 ]
+*   i = 5
+*   j = 10
+*/
