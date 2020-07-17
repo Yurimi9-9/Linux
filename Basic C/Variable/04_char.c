c103b2ea3c1c682b50e01da784f3c1c3ff61459a
Yurimi <59168302+Yurimi9-9@users.noreply.github.com>
2020-07-17 10:45:32 +0900

    Create 04_char.c

---
+++ Basic C/Variable/04_char.c
@@ -0,0 +1,19 @@
+/* Character Variable */
+#include <stdio.h>
+
+int main()
+{
+        char ch1, ch2; 
+
+        ch1 = 'a';    // 문자 'a' 저장
+        ch2 = 98;     // 문자 'b'의 정수값인 98 저장
+
+        printf("ch1 : %c   %d\n", ch1, ch1);
+        printf("ch2 : %c   %d\n", ch2, ch2);
+}
+
+/*
+*   [ 출력 ]
+*   ch1 : a    97
+*   ch2 : b    98
+*/
