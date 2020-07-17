5303ebb5a8963664ffe6472f987e6fd7487760b1
Yurimi <59168302+Yurimi9-9@users.noreply.github.com>
2020-07-17 20:39:01 +0900

    Create 1_basic_function.c

---
+++ Basic C/Function/1_basic_function.c
@@ -0,0 +1,22 @@
+/* Basic Structure of Function */
+#include <stdio.h>
+
+// 함수 func() 정의
+func()
+{
+        printf("func() run\n");
+}
+
+int main()
+{
+        printf("Before function call\n");
+        func();
+        printf("After function call\n");
+}
+
+/*
+*   [ 출력 ]
+*   Before function call
+*   func() run
+*   After function call
+*/
